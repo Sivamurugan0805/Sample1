1e724ef67724129fcc399aa38366bfb1a8b955d7
Sivamurugan-NKINFYX-Groups-Pvt-Ltd <sivamurugan-fs@nkinfyxgroups.in>
2025-11-29 11:47:36 +0530

    Second Version

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Practice1 from './Practice/Practice1';
+import Tryit from './Practice/Tryit';
+
+const app = () => {
+  return (
+    <div>
+      {/* <Practice1/> */}
+      <Tryit/>
+    </div>
+  )
+}
+
+export default app
